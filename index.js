e658b06ac2071d825fb10edcdf523cd2d2addc15
Josue Ramirez <josueramirez0110@gmail.com>
2022-05-02 10:59:25 -0500

    Finished project

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander');
+
+const carlo = new MissionCommander('Carlo');
+console.log(carlo.name);
+
+const fer = new MissionCommander('Fer');
+console.log(fer.name);
+
+const woopa = new MissionCommander('Woopa');
+console.log(woopa.name);
